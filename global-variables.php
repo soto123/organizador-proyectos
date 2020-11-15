d6a8e9fae237fcc38ff30cf4b03c0039785679ae
Borja hatchtemuco <soto.borja@gmail.com>
2020-11-14 21:02:05 -0300

    Create global-variables.php

---
+++ global-variables.php
@@ -0,0 +1,26 @@
+<?php 
+/*
+En este archivo se configuraran las variables globales para el proyecto
+
+*/
+class global_variables{
+	protected $servername="localhost";
+	protected $username="hatchtem_YcIbY1Ud";
+	protected $password="YcIbY1Ud";
+	protected $db="hatchtem_YcIbY1Ud";
+
+	function get_servername(){
+		return $this->servername;
+	}
+	function get_username(){
+		return $this->username;
+	}
+	function get_password(){
+		return $this->password;
+	}
+	function get_db(){
+		return $this->db;
+	}
+
+}
+ ?>
