451b1ff4ab74db3229dfb56bbaa0b7c7834ab67f
Borja hatchtemuco <soto.borja@gmail.com>
2020-11-15 19:13:29 -0300

    Create tablero.php

---
+++ tablero.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!doctype html>
+<html lang="es">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <title>Tablero - organizador</title>
+  </head>
+  <body>
+  	<div class="container-fluid">
+  		<div class="row">
+  			<div class="col-md-3 bg-dark pt-5 pb-5">
+  				<ul class="w-100 bg-info">
+  					<li>Item 1</li>
+  					<li>Item 2</li>
+  					<li>Item 3</li>
+  					<li>Item 4</li>
+  				</ul>
+  			</div>
+  			<div class="col-md-9">
+  				
+  			</div>
+  		</div>
+  	</div>
+    <h1>Hello, world!</h1>
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  </body>
+</html>
