f54d4af17dc2af4db5ba925ec262f10cfd935228
Borja hatchtemuco <soto.borja@gmail.com>
2020-11-25 13:07:30 -0300

    htaccess api
    Se agregarón las funciones en htacces y api.php
    Consultar atributo
    actualizar atributo

--- api.php
+++ api.php
@@ -8,8 +8,18 @@ if(isset($_POST) && $_POST != []){
 		${$_GET['objeto']} = new $_GET['objeto']();
 		if(isset($_GET['id'])){
 			${$_GET['objeto']}->get_by_id($_GET['id']);
-			var_dump(${$_GET['objeto']});
-			echo json_encode(${$_GET['objeto']}->object_to_json());
+			if(isset($_GET['att'])){
+				if(isset($_GET['update'])){//isset update
+					$func = 'set_'.$_GET['att'];
+					${$_GET['objeto']}->{$func}($_GET['val']);
+					var_dump(${$_GET['objeto']}->update());
+				}else{
+					$func = 'get_'.$_GET['att'];
+					echo ${$_GET['objeto']}->{$func}();
+				}
+			}else{
+				echo json_encode(${$_GET['objeto']}->object_to_json());	
+			}
 		}else{
 			$array = [];
 			foreach (${$_GET['objeto']}->get_all() as $objeto) {
